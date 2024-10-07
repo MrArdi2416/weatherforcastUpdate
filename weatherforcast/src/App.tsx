a5a28cc49c6be3443b21b72f9a942e662e5e270e
MrArdi2416 <dhavalardi1497@gmail.com>
2024-10-07 05:48:19 -0700

    Stagged Changes

--- weatherforcast/src/App.tsx
+++ weatherforcast/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
      <h1 className="text-3xl font-bold underline text-red-600">
       Simple React Typescript Tailwind Sample
     </h1>
